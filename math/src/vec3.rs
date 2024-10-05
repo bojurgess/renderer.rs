5a1f3c97e5ace5e022e027b53e303fc01277d36f
Joe Burgess <bojurgess@gmail.com>
2024-10-05 01:07:12 +0100

    zero + actually make the components public

--- math/src/vec3.rs
+++ math/src/vec3.rs
@@ -1,11 +1,17 @@
 #[derive(Debug, PartialEq, Clone, Copy)]
 pub struct Vec3 {
-    x: f32,
-    y: f32,
-    z: f32,
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
 }
 
 impl Vec3 {
+    pub const ZERO: Vec3 = Vec3 {
+        x: 0.0,
+        y: 0.0,
+        z: 0.0,
+    };
+
     pub fn new(x: f32, y: f32, z: f32) -> Vec3 {
         Self { x, y, z }
     }
